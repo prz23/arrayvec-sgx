e048c8f1630d9bee5910627b8d1514f02b29f360
prz23 <panruizheng@zju.edu.cn>
2021-07-13 11:10:20 +0800

    sgx

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,13 @@
 #![cfg_attr(not(feature="std"), no_std)]
 #![cfg_attr(feature="unstable-const-fn", feature(const_fn))]
 
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
 #[cfg(feature="serde")]
 extern crate serde;
 
